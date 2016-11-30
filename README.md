22ab039923d0c168f203490997ecbd792a367a7b
SrMouraSilva <mateus.moura@hotmail.com>
2016-11-30 16:41:31 -0300

    Add set pedal size by input[type='range']

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ https://PedalPi.github.io/Mod-sdk-HTML5
 
 This example uses css var + flexbox. [Google chrome contains a bug](https://bugs.chromium.org/p/chromium/issues/detail?id=618165).
 Test in firefox!
+
+Try online: http://dabblet.com/gist/0a91ce222d9e3e002bf40e328838c3af
--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function setColor(element, pedalSelector, cssSelector) {
   pedal.style.setProperty('--pedal-background', value);
   colorValue.textContent = colorValue.textContent.replace(/: #[0-9a-fA-F]*/g, ': ' + value);
 }
+
+function setSize(element, pedalSelector, cssSelector, property) {
+  let pedal = document.querySelector(pedalSelector);
+  let colorValue = document.querySelector(cssSelector + " code");
+  let value = element.value;
+
+  pedal.style.setProperty(property, value+"px");
+  colorValue.textContent = colorValue.textContent.replace(new RegExp(`${property}: [0-9]*px`), `${property}: ${value}px`);
+}
